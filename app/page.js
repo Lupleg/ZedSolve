92f58e64fafa4998e72b68a4e1a396a5719cbd08
marksikaundi <emmanuelmark117@gmail.com>
2023-12-09 17:00:22 +0200

    sample

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='text-black'>
-      <h1 className=''>Start</h1>
+    <div className=''>
+      <h1 className=''>Welcome to next gig</h1>
     </div>
   )
 }
